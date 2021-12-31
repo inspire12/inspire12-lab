14d09f16e303552e3fe7666f0691abf32d327bef
inspire12 <inspire12@ncsoft.com>
2021-12-31 23:05:40 +0900

    add web event flux

---
+++ src/main/java/com/inspire12/practice/lab/socket/ssevent/UserEmitService.java
@@ -0,0 +1,52 @@
+package com.inspire12.practice.lab.socket.ssevent;
+
+import com.inspire12.practice.lab.extern.jsonplaceholder.User;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Service
+public class UserEmitService {
+    private static final int REPEAT = 3;
+    private final Map<ResponseBodyEmitter, AtomicInteger> emitterCountMap = new HashMap<>();
+
+    public void add(ResponseBodyEmitter emitter) {
+        emitterCountMap.put(emitter, new AtomicInteger(0));
+    }
+
+
+    @Scheduled(fixedRate = 1000L)
+    public void emit() {
+        List<ResponseBodyEmitter> toBeRemoved = new ArrayList<>(emitterCountMap.size());
+        for (Map.Entry<ResponseBodyEmitter, AtomicInteger> entry : emitterCountMap.entrySet()) {
+            Integer count = entry.getValue().incrementAndGet();
+            User user = new RestTemplate().getForObject("https://jsonplaceholder.typicode.com/users/{id}", User.class, count);
+            ResponseBodyEmitter emitter = entry.getKey();
+            try {
+                emitter.send(user);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                toBeRemoved.add(emitter);
+            }
+
+            if (count >= REPEAT) {
+                toBeRemoved.add(emitter);
+            }
+        }
+
+        for (ResponseBodyEmitter emitter : toBeRemoved) {
+            emitterCountMap.remove(emitter);
+        }
+    }
+}
